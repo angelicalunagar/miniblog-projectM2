fab99fe069004069ffcb9a6669ad2c1428d67f82
angelicalunagar <angelica.lunagar@gmail.com>
2026-08-12 18:46:16 -0600

    se creó la URL http://localhost:3000/api-docs con YAML

--- src/app.js
+++ src/app.js
@@ -5,13 +5,16 @@ import cors from 'cors';
 import authorRoutes from './routes/authorRoutes.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import postRoutes from './routes/postRoutes.js';
-/* const commentRoutes = require('./routes/commentRoutes'); */
+import swaggerUi from "swagger-ui-express";
+import YAML from 'yamljs';
+
 
 const app = express();
-
+const swaggerDocument = YAML.load("./openapi.yaml");
 
 app.use(cors());
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Mount Routes
 app.use('/authors', authorRoutes);
