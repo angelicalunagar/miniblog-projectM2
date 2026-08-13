ab078ad14b602aefedfb5539f767dbb6f08396fc
angelicalunagar <angelica.lunagar@gmail.com>
2026-08-12 20:44:44 -0600

    agregré DATABASE_URL

--- src/config/db.js
+++ src/config/db.js
@@ -4,11 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
+  /* user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT, */
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
